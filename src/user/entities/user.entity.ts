639ab06222846b1149873df13227e072fb6b266b
minsubro <mmss3265@gmail.com>
2023-08-18 15:26:39 +0900

    npm 버전 업데이트 && postgres 연동

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('User')
+export class UserEntity {
+	@PrimaryGeneratedColumn()
+	user_idx: number;
+
+	@Column()
+	intra_id: string;
+
+	@Column()
+	nickname: string;
+
+	@Column()
+	avatar_path: string;
+
+	@Column()
+	status: boolean;
+
+	@Column()
+	auth: boolean;
+
+	@Column()
+	win: number;
+
+	@Column()
+	lose: number;
+
+	@Column()
+	is_leave: boolean;
+}
