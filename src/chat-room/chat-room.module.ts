3836c9ce21df8c5e686b8f92e1a720642bf7ac03
soong-e <yujelee@student.42seoul.kr>
2023-09-12 19:48:06 +0900

    feat: module에서 import함  #2

--- src/chat-room/chat-room.module.ts
+++ src/chat-room/chat-room.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChatRoomController } from './chat-room.controller';
 import { ChatRoomService } from './chat-room.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatRoom } from './entities/chat-room.entity';
+import { ChatRoomRepository } from './chat-room.repository';
 
 @Module({
-  controllers: [ChatRoomController],
-  providers: [ChatRoomService]
+	imports: [TypeOrmModule.forFeature([ChatRoom])],
+	controllers: [ChatRoomController],
+	providers: [ChatRoomService, ChatRoomRepository],
 })
 export class ChatRoomModule {}
