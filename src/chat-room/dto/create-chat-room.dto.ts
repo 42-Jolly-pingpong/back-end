bd14779dd7623f61f2e8b3663e9e53e92859aab1
soong-e <yujelee@student.42seoul.kr>
2023-09-12 19:28:37 +0900

    feat: create-chat-room dto 생성 #2

---
+++ src/chat-room/dto/create-chat-room.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ChatRoomType } from '../chat-room-type.enum';
+
+export class CreateChatRoomDto {
+	@ApiProperty({ description: '채팅방 이름' })
+	roomName: string;
+
+	@ApiProperty({ description: '채팅방 타입' })
+	roomType: ChatRoomType;
+
+	@ApiProperty({ description: '비밀번호' })
+	password: number | null;
+
+	@ApiProperty({ description: '최대 인원' })
+	maxPeople: number;
+}
