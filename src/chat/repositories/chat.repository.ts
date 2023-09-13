494df8c2de7110521222c1709753d80d2f1705c4
soong-e <yujelee@student.42seoul.kr>
2023-09-13 20:46:48 +0900

    chore: 안쓰는 임포트문 지움

--- src/chat/repositories/chat.repository.ts
+++ src/chat/repositories/chat.repository.ts
@@ -1,8 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
-import { PaticipantStatus } from '../enums/paticipant-status.enum';
-import { RoomAuth } from '../enums/room-auth.enum';
-import { ChatRoomDto } from '../dto/chat-room.dto';
 import { Chat } from '../entities/chat.entity';
 
 @Injectable()
