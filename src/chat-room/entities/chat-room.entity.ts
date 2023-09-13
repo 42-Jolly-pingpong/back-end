2968008dc5211d123c35ba7699af4550afae48e0
soong-e <yujelee@student.42seoul.kr>
2023-09-13 13:10:05 +0900

    refactor: 테이블 이름 스네이크 케이스로 수정

--- src/chat-room/entities/chat-room.entity.ts
+++ src/chat-room/entities/chat-room.entity.ts
@@ -9,27 +9,27 @@ import { ChatRoomType } from '../chat-room-type.enum';
 
 @Entity('ChatRoom')
 export class ChatRoom {
-	@PrimaryGeneratedColumn({ name: 'roomIdx' })
+	@PrimaryGeneratedColumn({ name: 'room_idx' })
 	roomIdx: number;
 
-	@Column({ name: 'roomName' })
+	@Column({ name: 'room_name' })
 	roomName: string;
 
-	@Column({ name: 'roomType' })
+	@Column({ name: 'room_type' })
 	roomType: ChatRoomType;
 
 	@Column({ name: 'password', nullable: true })
 	password: number | null;
 
-	@Column({ name: 'maxPeople' })
+	@Column({ name: 'max_people' })
 	maxPeople: number;
 
-	@UpdateDateColumn({ name: 'updateTime' })
+	@UpdateDateColumn({ name: 'update_time' })
 	updatedTime: Date;
 
 	@Column({ name: 'status', default: true })
 	status: boolean;
 
-	@Column({ name: 'currentPeople', default: 1 })
+	@Column({ name: 'current_people', default: 1 })
 	currentPeople: number;
 }
