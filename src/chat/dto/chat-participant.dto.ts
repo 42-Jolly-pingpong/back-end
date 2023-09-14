039beeab7497256519efacefca99d83f06d50d7a
soong-e <yujelee@student.42seoul.kr>
2023-09-14 21:13:48 +0900

    feat: Dto에 역할 추가

--- src/chat/dto/chat-participant.dto.ts
+++ src/chat/dto/chat-participant.dto.ts
@@ -1,6 +1,7 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { PaticipantStatus } from "../enums/paticipant-status.enum";
-import { UserInfoDTO } from "src/user/dto/userInfo.dto";
+import { ApiProperty } from '@nestjs/swagger';
+import { PaticipantStatus } from '../enums/paticipant-status.enum';
+import { UserInfoDTO } from 'src/user/dto/userInfo.dto';
+import { RoomAuth } from '../enums/room-auth.enum';
 
 export class ChatParticipantDto {
 	@ApiProperty({ description: '참여자 정보' })
@@ -8,6 +9,9 @@ export class ChatParticipantDto {
 
 	@ApiProperty({ description: '참여자 상태' })
 	status: PaticipantStatus;
+
+	@ApiProperty({ description: '참여자 역할' })
+	roomAuth: RoomAuth;
 
 	@ApiProperty({ description: '음소거 시간' })
 	muteExpirationTime: Date | null;
