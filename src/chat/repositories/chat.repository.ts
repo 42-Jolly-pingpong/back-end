9b72995292c6d4e804d7948d5ad5b2d4635be50f
soong-e <yujelee@student.42seoul.kr>
2023-09-14 19:27:32 +0900

    refactor: 코드 정리

--- src/chat/repositories/chat.repository.ts
+++ src/chat/repositories/chat.repository.ts
@@ -3,6 +3,7 @@ import { DataSource, Repository } from 'typeorm';
 import { Chat } from '../entities/chat.entity';
 import { ChatDto } from '../dto/chat.dto';
 import { plainToInstance } from 'class-transformer';
+import { CreateChatDto } from '../dto/create-chat.dto';
 
 @Injectable()
 export class ChatRepository extends Repository<Chat> {
@@ -13,9 +14,9 @@ export class ChatRepository extends Repository<Chat> {
 	async getChats(roomIdx: number): Promise<ChatDto[]> {
 		const query = this.createQueryBuilder('chat');
 
-		query.where('chat.roomIdx = :roomIdx', { roomIdx });
-
-		const chats = await query.getMany();
+		const chats = await query
+			.where('chat.roomIdx = :roomIdx', { roomIdx })
+			.getMany();
 
 		return plainToInstance(ChatDto, chats);
 	}
