44c5dd1adaf0bd4b978fe19003e64db5b8e2a511
soong-e <yujelee@student.42seoul.kr>
2023-09-14 21:13:04 +0900

    fix: 필드 타입 변경

--- src/chat/dto/chat.dto.ts
+++ src/chat/dto/chat.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { UserInfoDTO } from 'src/user/dto/userInfo.dto';
+import { ChatParticipantDto } from './chat-participant.dto';
 
 export class ChatDto {
 	@ApiProperty({ description: '챗 인덱스' })
 	chatIdx: number;
 
 	@ApiProperty({ description: '챗 보낸 사람' })
-	user: UserInfoDTO;
+	user: ChatParticipantDto;
 
 	@ApiProperty({ description: '챗 내용' })
 	content: string;
