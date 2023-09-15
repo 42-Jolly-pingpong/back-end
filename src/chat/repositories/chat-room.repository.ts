c7a42fa917f0e08ff7488ea7bf92ff3a60d5ef97
soong-e <yujelee@student.42seoul.kr>
2023-09-15 15:30:11 +0900

    refactor: 안쓰는 import문 제거

--- src/chat/repositories/chat-room.repository.ts
+++ src/chat/repositories/chat-room.repository.ts
@@ -4,7 +4,6 @@ import { DataSource, Repository } from 'typeorm';
 import { ChatRoom } from '../entities/chat-room.entity';
 import { ChatRoomDto } from '../dto/chat-room.dto';
 import { ChatRoomType } from '../enums/chat-room-type.enum';
-import { ChatParticipantDto } from '../dto/chat-participant.dto';
 
 @Injectable()
 export class ChatRoomRepository extends Repository<ChatRoom> {
