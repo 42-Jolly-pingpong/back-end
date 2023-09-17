7038cfd7a0c37d8da16b0bc76dcca941305f3ded
ebcode2021 <ebcode2021@gmail.com>
2023-09-17 22:13:39 +0900

    import GameHistory Module

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GameHistory } from './entities/game-history.entity';
 
 @Module({
-  controllers: [GameController],
-  providers: [GameService],
+	imports: [TypeOrmModule.forFeature([GameHistory])],
+	controllers: [GameController],
+	providers: [GameService],
 })
 export class GameModule {}
