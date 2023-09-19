b0b6d3b52cd3aee5fcb6aa57ab78310a2bea174e
ebcode2021 <ebcode2021@gmail.com>
2023-09-20 01:23:10 +0900

    api : update user

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,1 +1,15 @@
-export class UpdateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateUserDto {
+	@ApiProperty({ description: '닉네임' })
+	nickname: string;
+
+	@ApiProperty({ description: '프로필 파일 경로' })
+	avatarPath: string;
+
+	@ApiProperty({ description: '상태 메시지' })
+	statusMessage: string;
+
+	@ApiProperty({ description: '2차 인증 설정' })
+	auth: boolean;
+}
