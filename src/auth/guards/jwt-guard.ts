7af617a2efd9f51fb978187660a1783f615ba9d2
ebcode2021 <ebcode2021@gmail.com>
2023-10-02 16:22:32 +0900

    jwt handleRequest 추가

--- src/auth/guards/jwt-guard.ts
+++ src/auth/guards/jwt-guard.ts
@@ -2,4 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class AuthJwtGuard extends AuthGuard('jwt') {}
+export class AuthJwtGuard extends AuthGuard('jwt') {
+	handleRequest(err, user, info, context) {
+		if (err) {
+			const response = context.switchToHttp().getResponse();
+			response.redirect(
+				`${process.env.DOMAIN}:${process.env.FRONT_PORT}`
+			);
+		}
+		return user;
+	}
+}
