fd144150af78c9e0d5574f7e442a5f9a1d130af6
minsubro <mmss3265@gmail.com>
2023-10-11 14:47:50 +0900

    feat:게임소켓 게이트웨이 생성

--- src/game/game/game.gateway.ts
+++ src/game/game/game.gateway.ts
@@ -1,3 +1,4 @@
+import { ConsoleLogger } from '@nestjs/common';
 import {
 	MessageBody,
 	OnGatewayConnection,
@@ -10,14 +11,14 @@ import {
 import { Server, Socket } from 'socket.io';
 import { v4 as uuidv4 } from 'uuid';
 
-
 @WebSocketGateway(4242, { namespace: `game`, cors: { origin: '* ' } })
 export class GameGateway
 	implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
 	constructor() {}
 
-	private clientList: Map<string, Socket> = new Map<string, Socket>();
+	// 클라이언트의 정보를 보관해야할지 모르겠음
+	// private clientList: Map<string, Socket> = new Map<string, Socket>();
 	private waitQueue: Socket[] = [];
 
 	@WebSocketServer()
@@ -33,17 +34,17 @@ export class GameGateway
 	}
 
 	async handleConnection(client: Socket, ...args: any[]) {
-		console.log(`Client Connected : ${client.id}`);
-		this.clientList.set(client.id, client);
-		this.clientList.forEach((value, key) => {
-			console.log(`${key}: ${value}`);
-		});
-    return (null)
+		console.log(`클라이언트 연결됨 : ${client.id}`);
+		// this.clientList.set(client.id, client);
+		// this.clientList.forEach((value, key) => {
+		// 	console.log(`${key}: ${value.id}`);
+		// });
+		return null;
 	}
 
 	handleDisconnect(client: Socket) {
-		console.log(`Client Disconnected : ${client.id}`);
-		this.clientList.delete(client.id);
+		console.log(`클라이언트 연결 끊김 : ${client.id}`);
+		//	this.clientList.delete(client.id);
 	}
 
 	@SubscribeMessage('events')
@@ -54,26 +55,29 @@ export class GameGateway
 
 	@SubscribeMessage('matching')
 	gameMatchgin(client: Socket) {
-    console.log(this.waitQueue.length)
 		if (this.waitQueue.length < 1) {
-      this.waitQueue.push(client)
-      console.log('대기자 현황', this.waitQueue.forEach(client => {client.id}))
+			console.log('매칭 등록');
+			client.emit('gameMatching', 'success');
+			this.waitQueue.push(client);
+			this.waitQueue.forEach((client: Socket) => console.log(client.id))
+		} else {
+			console.log('game success')
+			const anotherClient: Socket = this.waitQueue.shift();
+			const roomName: string = uuidv4();
+			// 상대 클라이언트 소켓 접속 유무 확인하는 로직 필요
+			client.join(roomName);
+			anotherClient.join(roomName);
+			this.server.to(roomName).emit('gameStart');
 		}
-    else {
-      const anotherClient: Socket = this.waitQueue.shift()
-      const roomName: string = uuidv4()
-      client.join(roomName)
-      anotherClient.join(roomName)
-      this.server.to(roomName).emit('roomTest', '들어와잇어요')
-    }
 	}
 
-  @SubscribeMessage('cancel')
-  matchingCancel(client: Socket) {
-    const index = this.waitQueue.indexOf(client)
-    if (index !== -1) {
-      this.waitQueue.splice(index, 1)
-      console.log('대기자 현황', this.waitQueue)
-    }
-  }
+	@SubscribeMessage('cancel')
+	matchingCancel(client: Socket) {
+		const index = this.waitQueue.indexOf(client);
+		if (index !== -1) {
+			this.waitQueue.splice(index, 1);
+			client.emit('matchingCancel', 'success')
+			console.log('대기자 현황', this.waitQueue);
+		}
+	}
 }
