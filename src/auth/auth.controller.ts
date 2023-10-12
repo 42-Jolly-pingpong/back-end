a4632df9327769726d14f3eada6fb3a63078c7e9
ebcode2021 <ebcode2021@gmail.com>
2023-10-12 10:12:23 +0900

    필요없는 부분 주석

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -33,10 +33,12 @@ export class AuthController {
 		@Req() req: any,
 		@Res({ passthrough: true }) res: Response
 	): Promise<void> {
+		console.log('redirection 된 뒤');
 		const auth: AuthType = await this.authService.validateUser(req.user);
 
 		switch (auth) {
 			case AuthType.NOUSER:
+				console.log('회원가입을 해야합니다.');
 				res.cookie('user-data', JSON.stringify(req.user));
 				res.redirect(
 					`${process.env.DOMAIN}:${process.env.FRONT_PORT}/sign-up`
@@ -45,6 +47,7 @@ export class AuthController {
 			case AuthType.USERWITH2FA:
 				res.cookie('2FA', JSON.stringify(true));
 			case AuthType.USER:
+				console.log('이미 회원입니다.');
 				const token = await this.authService.createToken(req.user);
 				res.cookie('access-token', token);
 				res.redirect(`${process.env.DOMAIN}:${process.env.FRONT_PORT}`);
@@ -53,11 +56,16 @@ export class AuthController {
 
 	@ApiOperation({ summary: '회원가입' })
 	@Post('/signup')
-	async signup(@Body() formData: any, @Res() res: Response): Promise<void> {
+	async signup(
+		@Body() formData: any,
+		@Res({ passthrough: true }) res: Response
+	): Promise<void> {
 		await this.authService.signup(formData);
-		const token = await this.authService.createToken(formData.intraId);
+		const token = await this.authService.createToken(formData);
+		// [추후 수정할지말지..]
 		res.clearCookie('user-data');
 		res.cookie('access-token', token);
+		res.redirect(`${process.env.DOMAIN}:${process.env.FRONT_PORT}`);
 		res.status(200).end();
 	}
 
@@ -65,6 +73,7 @@ export class AuthController {
 	@UseGuards(AuthJwtGuard)
 	@Post('/user')
 	async user(@GetUser() user: UserDto): Promise<UserDto | null> {
+		console.log(user);
 		return user;
 	}
 }
--- src/auth/guards/jwt-guard.ts
+++ src/auth/guards/jwt-guard.ts
@@ -5,10 +5,7 @@ import { AuthGuard } from '@nestjs/passport';
 export class AuthJwtGuard extends AuthGuard('jwt') {
 	handleRequest(err, user, info, context) {
 		if (err || !user) {
-			const response = context.switchToHttp().getResponse();
-			response.redirect(
-				`${process.env.DOMAIN}:${process.env.FRONT_PORT}`
-			);
+			return;
 		}
 		return user;
 	}
