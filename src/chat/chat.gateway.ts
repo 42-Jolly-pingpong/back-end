9c0c70e513bd7daf2ef210773445e4d5a22ec30e
soong-e <yujelee@student.42seoul.kr>
2023-10-13 17:07:18 +0900

    feat: connect될 때 방에 join되게 함 #25

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,33 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import {
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	WebSocketGateway,
+	WebSocketServer,
+} from '@nestjs/websockets';
+import { ChatService } from 'src/chat/chat.service';
 
-@WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+@WebSocketGateway({
+	namespace: 'chat',
+	cors: {},
+})
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+	constructor(private readonly chatService: ChatService) {}
+
+	@WebSocketServer()
+	server: Server;
+
+	async handleConnection(client: Socket, ...args: any[]) {
+		const userId = client.handshake.auth.userId; //temp
+		const rooms = await this.chatService.inquireAllJoinedChatRooms(userId);
+
+		if (client.disconnect) {
+			rooms.map((room) => client.join(String(room.id)));
+		}
+		//status 수정
+	}
+
+	handleDisconnect(client: any) {
+		console.log('Method not implemented.');
+	}
 }
