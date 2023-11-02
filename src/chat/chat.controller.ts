ca7e78244e868054c63fd73a4ab25ef61649bf1c
soong-e <yujelee@student.42seoul.kr>
2023-11-02 16:13:40 +0900

    feat: 유저 가드 적용

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -8,12 +8,15 @@ import {
 	ValidationPipe,
 } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { GetUser } from 'src/auth/decorators/user-info';
+import { AuthJwtGuard } from 'src/auth/guards/jwt-guard';
 import { ChatService } from 'src/chat/chat.service';
 import { ChatParticipantDto } from 'src/chat/dto/chat-participant.dto';
 import { ChatRoomDto } from 'src/chat/dto/chat-room.dto';
 import { ChatDto } from 'src/chat/dto/chat.dto';
 import { DmDto } from 'src/chat/dto/dm.dto';
 import { RoomGuard } from 'src/chat/guards/room.guard';
+import { User } from 'src/user/entities/user.entity';
 
 @ApiTags('chat-controller')
 @Controller('chat-rooms')
@@ -22,26 +25,29 @@ export class ChatController {
 
 	@ApiOperation({ summary: '사용자가 참여하고있는 채팅방 목록 조회' })
 	@Get('')
-	async inquireJoinedChannels(): Promise<ChatRoomDto[]> {
-		return await this.chatService.inquireJoinedChannels(0); //temp
+	@UseGuards(AuthJwtGuard)
+	async inquireJoinedChannels(@GetUser() user: User): Promise<ChatRoomDto[]> {
+		return await this.chatService.inquireJoinedChannels(user.id);
 	}
 
 	@ApiOperation({ summary: 'dm 채팅방 리스트 조회' })
 	@Get('/dm')
+	@UseGuards(AuthJwtGuard)
 	@UsePipes(ValidationPipe)
-	async inquireDms(): Promise<DmDto[]> {
-		return await this.chatService.inquireDms(0); //temp
+	async inquireDms(@GetUser() user: User): Promise<DmDto[]> {
+		return await this.chatService.inquireDms(user.id);
 	}
 
 	@ApiOperation({ summary: '오픈 채팅방 목록 조회' })
 	@Get('/opened')
-	async inquireOpenedChatRoom(): Promise<ChatRoomDto[]> {
-		return await this.chatService.inquireOpenedChatRoom(0); //temp
+	@UseGuards(AuthJwtGuard)
+	async inquireOpenedChatRoom(@GetUser() user: User): Promise<ChatRoomDto[]> {
+		return await this.chatService.inquireOpenedChatRoom(user.id);
 	}
 
 	@ApiOperation({ summary: '채팅방 정보 조회' })
 	@Get('/:roomId')
-	@UseGuards(RoomGuard)
+	@UseGuards(AuthJwtGuard, RoomGuard)
 	async getChatRoomInfo(
 		@Param('roomId', ParseIntPipe) roomId: number
 	): Promise<ChatRoomDto> {
@@ -50,11 +56,12 @@ export class ChatController {
 
 	@ApiOperation({ summary: '채팅방 내부 챗 조회' })
 	@Get('/:roomId/chats')
-	@UseGuards(RoomGuard)
+	@UseGuards(AuthJwtGuard, RoomGuard)
 	async getChats(
+		@GetUser() user: User,
 		@Param('roomId', ParseIntPipe) roomId: number
 	): Promise<ChatDto[]> {
-		return await this.chatService.getChats(roomId, 0); //temp
+		return await this.chatService.getChats(roomId, user.id);
 	}
 
 	@ApiOperation({ summary: '채팅방 참여자 목록 조회' })
