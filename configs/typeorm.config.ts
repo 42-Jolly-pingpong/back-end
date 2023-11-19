2d1145d7ab60ca40146a209c3413616a8e438b61
minsubro <mmss3265@gmail.com>
2023-11-19 15:26:26 +0900

    fix: typeormconfig 적용하는 부분 수정

--- configs/typeorm.config.ts
+++ configs/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
 	type: 'postgres',
-	host: 'localhost',
+	host: process.env.DB_HOST || 'localhost',
 	port: +process.env.DB_PORT || 5432,
 	username: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
